715cb011be738fb257187afd9f25410a05323a4d
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2024-05-03 16:00:35 +0100

    Add dynamic list of landable bodies

--- src/Dfsseta.ApplyForLanding/Pages/Index.cshtml.cs
+++ src/Dfsseta.ApplyForLanding/Pages/Index.cshtml.cs
@@ -7,8 +7,18 @@ public class IndexModel(ILogger<IndexModel> logger) : BasePageModel("Apply for l
 {
     private readonly ILogger<IndexModel> _logger = logger;
 
+    public string[] LandableBodies { get; set; } = [];
+
     public void OnGet()
     {
-        
+        LandableBodies =
+        [
+            "Mars",
+            "Saturn (core)",
+            "International Space Station (ESA)",
+            "Tiangong space station",
+            "Earth's moon",
+            "Pluto"
+        ];
     }
 }
--- tests/Dfsseta.ApplyForLanding.UnitTests/Pages/IndexModelTests.cs
+++ tests/Dfsseta.ApplyForLanding.UnitTests/Pages/IndexModelTests.cs
@@ -6,11 +6,32 @@ namespace Dfsseta.ApplyForLanding.UnitTests.Pages;
 
 public class IndexModelTests
 {
+    private readonly IndexModel _sut;
+
+    public IndexModelTests()
+    {
+        var fakeLogger = A.Fake<Logger<IndexModel>>();
+        _sut = new IndexModel(fakeLogger);
+    }
+    
     [Fact]
     public void Heading_is_ApplyForLanding()
     {
-        var fakeLogger = A.Fake<Logger<IndexModel>>();
-        var sut = new IndexModel(fakeLogger);
-        sut.Heading.Should().Be("Apply for landing");
+        _sut.Heading.Should().Be("Apply for landing");
+    }
+
+    [Fact]
+    public void OnGet_should_set_LandableBodies()
+    {
+     _sut.OnGet();
+     _sut.LandableBodies.Should().Equal(
+     [
+         "Mars",
+         "Saturn (core)",
+         "International Space Station (ESA)",
+         "Tiangong space station",
+         "Earth's moon",
+         "Pluto"
+     ]);
     }
 }
